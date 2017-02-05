a581dc16b1f43b5250da7953d51540a557d855e0
Gabe <coverstory.clow@calbaptist.edu>
2017-02-04 20:07:14 -0800

    Table in progress

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Gabe on 2/4/2017.
  */
 public class Restaurant {
+
+    protected List<Table> tables;
+    protected List<Servers> waiters;
+    protected List<Party> parties;
+
+    public Restaurant() {
+        tables = new ArrayList();
+        waiters = new ArrayList();
+        parties = new ArrayList();
+    }
 }
--- src/Table.java
+++ src/Table.java
@@ -1,5 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by Gabe on 2/4/2017.
+ * @author Created by Gabe on 2/4/2017.
  */
-public class Table {
+public class Table{
+
+    /**
+     * variables for immutability delclared below
+     */
+
+    private final int id = 0;
+    private final int seats = 0;
+    private final boolean taken = true;
+
+    /**
+     * constructor, made to be immutable
+     */
+
+
+    public Table(int id, int seats, boolean taken) {
+        int tableID = id;
+        int tableseats = seats;
+        boolean tabletaken = false;
+
+    }
+
+
+
+    /** @return whether the table is taken or is free*/
+    boolean isTaken() {
+        return this.taken;
+    }
+
+    /**returns a text representation of the table */
+    public String toString(){
+        return ( id + seats + Boolean.toString(isTaken()));
+    }
+
+    /** */
+    public Table getTableByID(int id)throws IllegalArgumentException{
+    for( Table i: tables){
+    if(i.id == id){
+        return i;
+    }
+    else
+    }
+    }
+
+
 }
